84ee14fe2934c89d32bb175e4245e74b606af9d4
aabr2612 <aabr2612@gmail.com>
2024-11-24 02:49:21 +0500

    Search functionality fixed

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title'
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products,search,showSearch } = useContext(ShopContext);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
@@ -31,6 +31,10 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if(showSearch && search)
+    {
+      productsCopy = productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
@@ -60,7 +64,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory])
+  }, [category, subCategory,search,showSearch])
   useEffect(() => {
     sortProduct();
   }, [sortType])
